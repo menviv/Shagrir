bfd3ef07283a0265a34fc28db41de2fa9711a252
Menashe <menashe.gezelkopy@gmail.com>
2017-05-04 18:17:51 +0300

    version 23

--- messages/index.js
+++ messages/index.js
@@ -337,7 +337,10 @@ bot.dialog('/homeMenu', [
     },
     function (session, results) {
 
-        session.userData.helpSubject = results.response.entity;
+        session.userData.helpSubject = helpData[results.response.entity];
+
+        var helpSubject = helpData[results.response.entity];
+        session.send("We sold %(url)d units for a total of %(total)s.", helpSubject); 
 
        // session.send(results.response[0].url);
 
